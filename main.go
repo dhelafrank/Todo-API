16545a32b1b4d35b8194b4a2eb0ccd9cc53a0ddc
dhelafrank <ubongabasijerome8@gmail.com>
2024-06-25 02:36:57 +0100

    Init: Type Declarations

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"todo-api/types"
+)
+
+var todos = []types.Todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Code Go", Completed: false},
+	{ID: "3", Item: "Read Book", Completed: false},
+}
---
+++ types/todo.go
@@ -0,0 +1,7 @@
+package types
+
+type Todo struct {
+	ID        string
+	Item      string
+	Completed bool
+}
